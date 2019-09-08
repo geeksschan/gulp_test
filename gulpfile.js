6b7d13c89885a2a25f00d7cea32db9ca50d43467
USER <user@USERui-MacBookPro.local>
2019-09-09 01:28:45 +0900

    테스트 중

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,16 @@ const { src, dest, watch, series } = require('gulp');
 
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 function runBrowserSync(cb) {
   browserSync.init({
-    watch: true,
     server: {
       baseDir: "./src",
       directory: true
-    }
+    },
+    open: "external"
   });
 
   cb();
@@ -17,15 +19,25 @@ function runBrowserSync(cb) {
 
 function runWatch(cb) {
   watch('./src/scss/**/*.scss', runSass);
-  // watch('./src/*.html').on('change', browserSync.reload);
+  watch('./src/*.html').on('change', browserSync.reload);
   cb();
 }
 
 function runSass(cb) {
-  return src('./src/scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError()))
-        .pipe(dest('./src/css'))
+  const options = {
+    sass: {
+      outputStyle: 'expanded',
+      indentType: 'tab',
+      indentWidth: 1
+    }
+  };
+
+  return src('./src/scss/**/*.scss', { sourcemaps: true })
+        .pipe(sass.sync(options.sass).on('error', sass.logError))
+        .pipe(postcss([autoprefixer()]))
+        .pipe(dest('./src/css', { sourcemaps: '.' }))
         .pipe(browserSync.stream());
 }
 
-exports.browser = series(runBrowserSync, runWatch);
+exports.default = series(runBrowserSync, runSass, runWatch);
+exports.test = runSass;
